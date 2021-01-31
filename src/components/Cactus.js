14a2909874b97c153963699819a7818b34a96b9d
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-01-31 17:25:30 +0200

    fix: add name for cactuses

--- src/components/Cactus.js
+++ src/components/Cactus.js
@@ -2,10 +2,11 @@ import { Container, Sprite } from "pixi.js";
 import gsap from "gsap";
 
 export default class Cactus extends Container {
-  constructor(image) {
+  constructor(name) {
     super();
+    this.name = name;
     this._elements = new Container();
-    this._body = Sprite.from(image);
+    this._body = Sprite.from(name);
     this._rotation = -0.2;
     this._body.anchor.set(0.5, 1);
     this._body.rotation = this._rotation;
